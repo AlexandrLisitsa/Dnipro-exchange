eb32a23cf0268c925b8591dbe5db4810232f0487
AlexandrLisitsa <wypik28@gmail.com>
2022-11-06 01:11:26 +0200

    My rates (#5)
    * auth
    
    * my rates
    
    Co-authored-by: alex.lisitsa <alex.lisitsa@getgsi.com>

--- src/main/java/exchange/statemachine/transitions/MyRates.java
+++ src/main/java/exchange/statemachine/transitions/MyRates.java
@@ -1,17 +1,26 @@
 package exchange.statemachine.transitions;
 
+import exchange.service.rest.ClientHttpService;
 import exchange.statemachine.Event;
+import exchange.statemachine.Payload;
 import exchange.statemachine.State;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.annotation.WithStateMachine;
 import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.util.Collections;
+import java.util.Map;
 
 @WithStateMachine
 public class MyRates extends Transition {
 
     @Autowired
     private MainMenu mainMenu;
+    @Autowired
+    private ClientHttpService clientHttpService;
 
     @Override
     public void configure(StateMachineTransitionConfigurer<State, Event> transitions) throws Exception {
@@ -35,6 +44,25 @@ public class MyRates extends Transition {
 
     @Override
     public void execute(StateContext<State, Event> context) {
+        String phone = (String) context.getExtendedState().getVariables().get("phone");
+        Map<String, ClientHttpService.CurrencyValue> clientRates = clientHttpService.getClientRates(phone);
 
+        StringBuilder ratesMessage = new StringBuilder("<<< Мои курсы >>>").append("\n\n");
+        clientRates.forEach((currency, values) -> {
+            ratesMessage.append("         ").append(currency.toUpperCase()).append("\n");
+            ratesMessage.append("Покупка: ").append(values.getBuy()).append("\n");
+            ratesMessage.append("Продажа: ").append(values.getSell()).append("\n\n");
+        });
+
+        InlineKeyboardButton mainMenuButton = InlineKeyboardButton.builder()
+                .text("Назад")
+                .callbackData(Event.MAIN_MENU.toString())
+                .build();
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        markup.setKeyboard(Collections.singletonList(Collections.singletonList(mainMenuButton)));
+
+        Payload payload = (Payload) context.getExtendedState().getVariables().get("payload");
+
+        botService.sendMessage(payload.getChatId(), ratesMessage.toString(), markup);
     }
 }
