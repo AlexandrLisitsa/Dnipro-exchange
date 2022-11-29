6a5d308f3f93834f4025917475f56761c7966d0d
alex.lisitsa <alex.lisitsa@getgsi.com>
2022-11-29 02:22:41 +0200

    h2-db

---
+++ src/main/java/exchange/db/repository/UserRepo.java
@@ -0,0 +1,10 @@
+package exchange.db.repository;
+
+import exchange.db.entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<Client, Long> {
+    Client getClientByUserId(String userId);
+}
